accb82004561580cbe5c17c435bbc90debbe1ac1
KasihPutriSuarli <114201387+KasihPutriSuarli@users.noreply.github.com>
2023-05-31 12:29:30 +0700

    UTS PBO2 31 MEI 2023

---
+++ src/UTS_KasihPutriSuarli_ATMMini/InformasiSaldoModel.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UTS_KasihPutriSuarli_ATMMini;
+import UTS_KasihPutriSuarli_ATMMini.TransferModel;
+
+/**
+ *
+ * @author user
+ */
+public class InformasiSaldoModel {
+    String saldo="20000000";
+    
+
+    public String getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(String saldo) {
+        this.saldo = saldo;
+    }
+    
+}
